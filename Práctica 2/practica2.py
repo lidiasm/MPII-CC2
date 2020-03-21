ee971cb422834ecb0f52b68777f7045a129f8246
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-03-21 21:17:15 +0100

    Procesado y unificado el dataframe

--- Práctica 2/practica2.py
+++ Práctica 2/practica2.py
@@ -4,6 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 import requests
+import pandas
 
 default_args = {
     'owner': 'airflow',
@@ -15,6 +16,26 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5), # Cada cuanto se reintenta la ejecución.
 }
+
+############################ FUNCIONES DE PYTHON ############################
+
+def get_datos():
+    # Leemos los dos ficheros csv
+    df_humedad = pandas.read_csv('/tmp/workflow/humidity.csv')
+    df_temperatura = pandas.read_csv('/tmp/workflow/temperature.csv')
+    # Extraemos la columna de la ciudad de San Francisco de humedad y temperatura
+    humedad_sf = df_humedad['San Francisco']
+    temperatura_sf = df_temperatura['San Francisco']
+    # Obtenemos la columna de las fechas porque es la misma en ambos datasets
+    datetime = df_humedad['datetime']
+    # Formamos el nuevo dataframe unificado
+    d = {'DATE':df_humedad['datetime'], 'TEMP':temperatura_sf, 'HUM':humedad_sf}
+    dataframe = pandas.DataFrame(data=d)
+    # Escribimos el nuevo dataframe en un fichero csv
+    ## Con index=False le decimos que no escriba el número de las filas
+    dataframe.to_csv('/tmp/workflow/dataframe.csv', index=False)
+
+############################ FUNCIONES DE PYTHON ############################
 
 # Inicializamos el grafo de tareas.
 dag = DAG(
@@ -46,13 +67,21 @@ CapturaDatosTemperatura = BashOperator(
                             dag=dag
                             )
 # DescomprimirDatos: tarea encargada de descomprimir ambos ficheros.
-# Con la opción "-d" especificamos la ruta donde queremos que descomprima los ficheros
+## Con la opción "-d" especificamos la ruta donde queremos que descomprima los ficheros.
+## Con la opción "-o" le indicamos que sobreescriba los ficheros sin preguntar.
 DescomprimirDatos = BashOperator(
                         task_id='descomprimir_datos',
                         depends_on_past=False,
-                        bash_command='unzip /tmp/workflow/temperature.csv.zip -d /tmp/workflow ; unzip /tmp/workflow/humidity.csv.zip -d /tmp/workflow',
+                        bash_command='unzip -o /tmp/workflow/temperature.csv.zip -d /tmp/workflow ; unzip -o /tmp/workflow/humidity.csv.zip -d /tmp/workflow',
                         dag=dag
                         )
+# UnificarDatos: tarea encargada de extraer la humedad y temperatura de San Francisco así como la hora a la que se ha medido.
+UnificarDatos = PythonOperator(
+                    task_id='unificar_datos',
+                    python_callable=get_datos,
+                    op_kwargs={},
+                    dag=dag
+                    )
 
 ## ORDEN DE EJECUCIÓN DE TAREAS
-PrepararEntorno >> [CapturaDatosHumedad, CapturaDatosTemperatura] >> DescomprimirDatos
+PrepararEntorno >> [CapturaDatosHumedad, CapturaDatosTemperatura] >> DescomprimirDatos >> UnificarDatos
